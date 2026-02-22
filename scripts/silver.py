e78b4430897ec7e9553f8309403377219276c0f5
Samuel Freitas <samuelfreitas2998@gmail.com>
2026-02-22 08:46:22 -0300

    Criando camada Silver

---
+++ scripts/silver.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from pathlib import Path
+
+#Caminhos
+
+bronze_path = Path("data/bronze/PRESTADORES_SERVICOS.parquet")
+silver_path = Path("data/silver/PRESTADORES_SERVICOS.parquet")
+
