1196b11e508de5e774375a8c2361b49256da01d0
Samuel Freitas <samuelfreitas2998@gmail.com>
2026-02-22 09:00:19 -0300

    Adiciona o caminho

--- scripts/silver.py
+++ scripts/silver.py
@@ -6,3 +6,12 @@ from pathlib import Path
 bronze_path = Path("data/bronze/PRESTADORES_SERVICOS.parquet")
 silver_path = Path("data/silver/PRESTADORES_SERVICOS.parquet")
 
+def main():
+  print(f"Carregando dados da camada bronze...")
+  df = pd.read_parquet(bronze_path)
+
+  print("Tratando os dados na camada Silver...")
+  
+  
+ 
+
